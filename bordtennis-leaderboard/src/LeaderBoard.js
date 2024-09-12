906fe5a99f00e040dd03d21b115d11b0ada9ebd1
oleerik.nord <oleerik.nord@gmail.com>
2024-09-12 20:50:04 +0100

    dummy

--- bordtennis-leaderboard/src/LeaderBoard.js
+++ bordtennis-leaderboard/src/LeaderBoard.js
@@ -18,6 +18,7 @@ function Leaderboard() {
         ...doc.data(),
       }));
       // Sort players by number of wins in descending order
+      //Dummy
       const sortedPlayers = playersList.sort((a, b) => b.matchesWon - a.matchesWon);
       setSpillere(sortedPlayers);
     };
